2c43e999606ffb2a609c1053b3aef81db95c39a0
Victor Mac Laptop <175169337+itsvictoroyedeji@users.noreply.github.com>
2025-03-18 05:11:27 -0400

    feat: Modules switch when tab is clicked, plus...
    ...tab is highlighted depending on which page is
    active.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,49 @@ import main from './pages/main.js';
 import menu from './pages/menu.js';
 import contact from './pages/contact.js';
 
-// Call Main page
-contact();
-// menu();
-// main();
+// Get DOM element for buttons
+const buttons = document.querySelector(".tab-list");
+
+// Get DOM element for #content
+const contentDiv = document.getElementById("content");
+
+// Add Event Listener for all buttons (capturing method)
+buttons.addEventListener("click", loadTabPage);
+
+function loadTabPage(e) {
+  let buttonId = e.target.getAttribute("id");
+
+  // tab button logic
+  switch (buttonId) {
+    case "home":
+      contentDiv.textContent = '';
+      main();
+      break;
+    case "menu":
+      contentDiv.textContent = '';
+      menu();
+      break;
+    case "contact":
+      contentDiv.textContent = '';
+      contact();
+      break;
+  };
+};
+
+// Highlight tab when clicked
+buttons.addEventListener ("click", addTabCover);
+
+function addTabCover(e) {
+
+  // Get all nav buttons
+  const allNavButtons = document.querySelectorAll('.tab-list button');
+
+  // Remove the class attribute from all buttons when button is clicked
+  allNavButtons.forEach(button => button.classList.remove("active"));
+
+  // Add active to clicked button
+  e.target.classList.add("active");
+}
+
+ // Run initial homepage
+ main();
